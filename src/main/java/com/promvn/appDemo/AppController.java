7ef7528091f4838e9cc2d00c2414668fad8eede7
snx <snx1029@163.com>
2020-08-25 21:47:33 +0800

    project maven

---
+++ src/main/java/com/promvn/appDemo/AppController.java
@@ -0,0 +1,15 @@
+package com.promvn.appDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class AppController {
+    @RequestMapping(" ")
+    public ModelAndView index()
+    {
+
+        return new ModelAndView("/index");
+    }
+}
---
+++ src/main/webapp/jsp/index.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Temporary Title</title>
+</head>
+<body>
+<p>Hello Spring Boot!</p>
+</body>
+</html>
